9850499d211c99587b1057a5695858e9bdb951ab
Mathijs Markvoort <75422017+Gamermathijs@users.noreply.github.com>
2022-02-27 20:06:56 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+for index in range(10):
+    if 0 == 0:
+        basic.show_number(0)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    for (let index = 0; index <= 9; index++) {
+        if (0 == 0) {
+            basic.showNumber(index)
+        }
+    }
+})
